30d8e7c358c31af3ae011c3190b7bcdf776d64ce
david.o <david.otieno@ramtech.co.uk>
2019-02-11 15:45:21 +0000

    draft interface

---
+++ src/android/key_events.java
@@ -0,0 +1,51 @@
+package uk.co.nightsnowgames.cordova.key_events;
+
+import android.app.Activity;
+import android.content.Context;
+import org.apache.cordova.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class key_events extends CordovaPlugin
+{
+    private CallbackContext cordova_cb_context;
+	private boolean cordova_ready = false;
+
+	private void cordova_cb_json(JSONObject arg, boolean success)
+	{
+		if(cordova_ready == true)
+		{
+			if(cordova_cb_context != null)
+			{
+				PluginResult result = new PluginResult(success ? PluginResult.Status.OK : PluginResult.Status.ERROR, arg);
+				result.setKeepCallback(true);
+				cordova_cb_context.sendPluginResult(result);
+			}
+		}
+		else
+		{
+			Log.d(TAG, "java dropping event, cordova not ready");
+		}
+	}
+
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callback_context) throws JSONException
+	{
+		Activity activity = this.cordova.getActivity();
+		JSONObject result = new JSONObject();
+
+		if("init".equals(action))
+		{
+			cordova_ready = true;
+			result.put("event_type", "event_type_init");
+			cordova_cb_json(result, true);
+		}
+		else
+		{
+			result.put("error_string", action + " is not a supported action");
+			cordova_cb_json(result, false);
+		}
+
+		return false;
+	}
+}
--- www/cordova-key-events.js
+++ www/cordova-key-events.js
@@ -1,5 +1,38 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'cordova-key-events', 'coolMethod', [arg0]);
+var key_events =
+{
+	event_cb: null,
+
+	init: function(p_event_cb)
+	{
+		key_events.event_cb = p_event_cb;
+
+		exec(key_events.success, key_events.error, "cordova-key-events", "init", []);
+	},
+
+	success: function(arg)
+	{
+		switch(arg["event_type"])
+		{
+			case "event_type_key_event":
+				key_events.event_cb.key_event(parseInt(arg["key_event"]));
+				break;
+
+			case "event_type_init":
+				console.log("key_events: init SUCCESS");
+				break;
+
+			default:
+				console.log("key_events: unexpected success event: "+arg["event_type"]);
+				break;
+		}
+	},
+
+	error: function(arg)
+	{
+		console.log("key_events: error: "+arg["error_string"]);
+	},
 };
+
+module.exports = key_events;
